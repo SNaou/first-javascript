d46689445b34f6c33d21d111ff7d1abb12bf09b8
SNaou <S.Naou@icloud.com>
2023-01-09 14:34:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+console.log("Hello, Winc Academy!");
+let name = "Soumaya";
+console.log(name);
+//No, it is not possible to create a variable with the name "if" in JavaScript because "if" is a reserved word in the language.
+
+let sum = 4 + 4;
+console.log("sum:", sum);
+let sumWithStrings = "4" + "4"; //8
+
+console.log("sum with strings:", sumWithStrings);
+let subtraction = 4 - 4;
+console.log("subtraction:", subtraction)//0
+
+let multiplication = 4 * 4;
+console.log("multiplication:", multiplication);//16
+
+let division = 4 / 4;
+console.log("division:", division);//1
+
+let modulus = 4 % 4;//0
+
+console.log("modulus:", modulus);
+let age = 37;
+
+console.log(typeof age);
+let ageAsString = "37";
+console.log(typeof ageAsString);//string
+
+//this is a 
+
+
+
